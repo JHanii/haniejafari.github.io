4cd05e6a4b698d4d4b3a7f156a08be560aff4845
hani <hani.j.2000@gmail.com>
2020-02-15 09:35:57 +0330

    tarin

---
+++ class/C1/cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Reverce<_Type>(_Type[] list)
+            {
+                for(int i=0, j=list.length -1; i<list.length/2; i++ , j--)
+                {
+                    auto temp = list[i];
+                    list[i] = list[j];
+                    list[j] = list[i];
+                }
+            }
+            static void Print()
+            {
+                for (int i=0; i<list.length; i++)   System.Console.Writeline(list[i]);
+            }
+            System.Console.ReadKey();
+        }
+    }
+    
+}
